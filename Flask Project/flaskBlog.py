451f59f876c43e29262e5ff2bc0567ad7d22c1a7
shikherjaitly <shikher.jaitly@gmail.com>
2022-03-03 20:28:30 +0530

    Day-1: 3rd March 2022

--- Flask Project/flaskBlog.py
+++ Flask Project/flaskBlog.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Shikher Jaitly',
+        'title': 'Blog Post 1',
+        'content':'First Blog content',
+        'date_Posted':'March 3rd 2022'
+    },
+    {
+        'author': 'Shikher Jaitly',
+        'title': 'Blog Post 2',
+        'content':'Second Blog content',
+        'date_Posted':'March 4th 2022'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template('home.html',posts =posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='localhost')
